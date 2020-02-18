9c9b08b0c79cdf1c0dac0a91a911d20268bd7e47
Leonardo Curry <leonardocurry@yahoo.com>
2020-02-17 23:03:40 -0800

    clean up some of the code

--- tetris.js
+++ tetris.js
@@ -1,9 +1,9 @@
-const cvs = document.getElementById('canvas');
-const ctx = cvs.msGetInputContext('2d');
+const cvs = document.getElementById("tetris");
+const ctx = cvs.getContext("2d");
 
 const SQ = squareSize = 20;
 const ROW = 20;
-const COLUMN = 10;
+const COL = COLUMN = 10;
 const VACANT = "white"; // color of an empty square
 
 // draw square
@@ -240,6 +240,25 @@ Piece.prototype.collision = function(x,y, piece){
         }
     }
     return false;
+}
+
+// CONTROL the piece
+
+document.addEventListener("keydown", CONTROL);
+
+function CONTROL(event){
+    if(event.keyCode == 37){
+        piece.moveLeft();
+        dropStart = Data.now();
+    }else if (event.keyCode == 38){
+        piece.rotate();
+        dropStart = Data.now();
+    }else if (event.keyCode == 39){
+        piece.moveRight();
+        dropStart = Data.now();
+    }else if (event.keyCode == 40){
+        piece.moveDown();
+    }
 }
 
 // drop the piece every 1sec
